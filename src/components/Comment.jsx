623946f813d07aa6053a9c927d743f6277c91f2b
Veronica <almeida.vrc@gmail.com>
2023-01-09 23:19:22 -0300

    Adiciona funcionalidade de Deletar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,10 @@ import { Trash, ThumbsUp } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -20,7 +23,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
